300a8c923c6e5350886aa479f8e895f731b2b382
pitprok <44905554+pitprok@users.noreply.github.com>
2018-12-12 12:46:52 +0200

    initial commit

---
+++ src/java/Servlets/CertificationSave.java
@@ -0,0 +1,101 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Servlets;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Paths;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+
+/**
+ *
+ * @author pitpr
+ */
+@MultipartConfig(fileSizeThreshold = 1024 * 1024,
+        maxFileSize = 1024 * 1024 * 10, maxRequestSize = 1024 * 1024 * 10)
+public class CertificationSave extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            String prefix = getServletContext().getRealPath("/");
+            Part filePart = request.getPart("certification"); // Retrieves <input type="file" name="profileImage">
+            String fileName = Paths.get(filePart.getSubmittedFileName()).getFileName().toString(); // Internet Explorer getSubmittedFileName fix.
+
+            new File(prefix + "certifications").mkdirs(); //creates the directory folder if it doesn't exist
+            fileName = fileName.replaceAll("\\s", "");
+            File tmpDir = new File(prefix + "/certifications/" + fileName);
+            boolean exists = tmpDir.exists();
+
+            System.out.println("Directory:" + prefix);
+            System.out.println(fileName);
+            if (exists) {
+                out.println("The certification already exists");
+            } else {
+                System.out.println("1");
+                filePart.write(prefix + "/certifications/" + fileName);//save file to disk
+                System.out.println("2");
+                out.println("The certification was saved successfully");
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
